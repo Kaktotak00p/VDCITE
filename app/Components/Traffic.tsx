88aa492760ddaf32fe980b9c340dea9f1f3af6f8
Kuzin, Mikle <m.kuzin@student.maastrichtuniversity.nl>
2024-03-15 17:11:34 +0100

    huy

---
+++ app/Components/Traffic.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import '../../public/Styles/Traffic.css';
+import '../../public/Styles/globals.css';
+
+const trafic = () => {
+    return (
+        <div className='outerConatiner'>
+            <div className="mainContainer">
+                <h1>НАШИ ПРЕИМУЩЕСТВА</h1>
+            </div>
+            <div className='layoutContainer'>
+                <div className='imageContainer'>
+                    <img src='https://static.wixstatic.com/media/1c47da_8d9bbf50214d4e72899b1357a125c2c7~mv2.png/v1/crop/x_175,y_81,w_753,h_896/fill/w_249,h_296,fp_0.50_0.50,q_85,usm_0.66_1.00_0.01,enc_auto/1c47da_8d9bbf50214d4e72899b1357a125c2c7~mv2.png'></img>
+                </div>
+                <div className="cardContainer">
+                    <img src="https://static.wixstatic.com/media/1c47da_f5ac5ab022434fe0b08a4c7244fa63e4~mv2.png/v1/crop/x_529,y_47,w_934,h_646/fill/w_266,h_184,fp_0.50_0.50,q_85,usm_0.66_1.00_0.01,enc_auto/CardGreenShort.png" alt="Descriptive Alt Text" />
+                    <div className="overlayText">Лей Без Лимитов</div>
+                </div>
+                <div className="cardContainer">
+                    <img src="https://static.wixstatic.com/media/1c47da_f5ac5ab022434fe0b08a4c7244fa63e4~mv2.png/v1/crop/x_529,y_47,w_934,h_646/fill/w_266,h_184,fp_0.50_0.50,q_85,usm_0.66_1.00_0.01,enc_auto/CardGreenShort.png" alt="Descriptive Alt Text" />
+                    <div className="overlayText">Высокие Ставки</div>
+                </div>
+                <div className="cardContainer">
+                    <img src="https://static.wixstatic.com/media/1c47da_f5ac5ab022434fe0b08a4c7244fa63e4~mv2.png/v1/crop/x_529,y_47,w_934,h_646/fill/w_266,h_184,fp_0.50_0.50,q_85,usm_0.66_1.00_0.01,enc_auto/CardGreenShort.png" alt="Descriptive Alt Text" />
+                    <div className="overlayText">Дружная Команда</div>
+                </div>
+                <div className="cardContainer">
+                    <img src="https://static.wixstatic.com/media/1c47da_f5ac5ab022434fe0b08a4c7244fa63e4~mv2.png/v1/crop/x_529,y_47,w_934,h_646/fill/w_266,h_184,fp_0.50_0.50,q_85,usm_0.66_1.00_0.01,enc_auto/CardGreenShort.png" alt="Descriptive Alt Text" />
+                    <div className="overlayText">Лучшее качество траффика</div>
+                </div>
+                <div className="cardContainer">
+                    <img src="https://static.wixstatic.com/media/1c47da_f5ac5ab022434fe0b08a4c7244fa63e4~mv2.png/v1/crop/x_529,y_47,w_934,h_646/fill/w_266,h_184,fp_0.50_0.50,q_85,usm_0.66_1.00_0.01,enc_auto/CardGreenShort.png" alt="Descriptive Alt Text" />
+                    <div className="overlayText">Лучшее качество траффика</div>
+                </div>
+                <div className="cardContainer">
+                    <img src="https://static.wixstatic.com/media/1c47da_f5ac5ab022434fe0b08a4c7244fa63e4~mv2.png/v1/crop/x_529,y_47,w_934,h_646/fill/w_266,h_184,fp_0.50_0.50,q_85,usm_0.66_1.00_0.01,enc_auto/CardGreenShort.png" alt="Descriptive Alt Text" />
+                    <div className="overlayText">Лучшее качество траффика</div>
+                </div>
+                <div className="cardContainer">
+                    <img src="https://static.wixstatic.com/media/1c47da_f5ac5ab022434fe0b08a4c7244fa63e4~mv2.png/v1/crop/x_529,y_47,w_934,h_646/fill/w_266,h_184,fp_0.50_0.50,q_85,usm_0.66_1.00_0.01,enc_auto/CardGreenShort.png" alt="Descriptive Alt Text" />
+                    <div className="overlayText">Лучшее качество траффика</div>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default trafic;
