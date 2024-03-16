6b25b31e82259e636e23df2d6281eca6ca84f672
Kuzin, Mikle <m.kuzin@student.maastrichtuniversity.nl>
2024-03-16 14:01:36 +0100

    первые шаги к решении проблемы с раутингом

---
+++ app/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Landing from "./Components/Landing";
+import Trafic from "./Components/Traffic";
+import HeadHunter from "./Components/HeadHunter"
+import TrafSources from "./Components/TrafficSources"
+import Partners from "./Components/Partners"
+import type { ReactElement } from 'react'
+
+
+import dynamic from 'next/dynamic';
+import RootLayout from './layout';
+
+const ContactFormClient = dynamic(() => import('./Components/ContactForm.client'), { ssr: false });
+
+
+const Home = () => {
+  return (
+    <>
+    <Landing />
+    <Trafic />
+    <HeadHunter />
+    <TrafSources />
+    <Partners />
+    <ContactFormClient />
+    </>
+    )
+};
+
+
+export default Home;
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import Landing from "./Components/Landing";
-import Trafic from "./Components/Traffic";
-import HeadHunter from "./Components/HeadHunter"
-import TrafSources from "./Components/TrafficSources"
-import Partners from "./Components/Partners"
-
-import dynamic from 'next/dynamic';
-
-const ContactFormClient = dynamic(() => import('./Components/ContactForm.client'), { ssr: false });
+import Home from './index';
+import RootLayout from './layout';
 
 
-const Home = () => {
+const indx = () => {
   return (
     <>
-    <Landing />
-    <Trafic />
-    <HeadHunter />
-    <TrafSources />
-    <Partners />
-    <ContactFormClient />
+  <Home />
     </>
     )
 };
 
-export default Home;
+
+export default indx;
