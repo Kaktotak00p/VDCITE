993629077d86fb8082879556652546095158eb32
Kuzin, Mikle (Stud. DACS) <m.kuzin@student.maastrichtuniversity.nl>
2024-03-19 15:39:28 +0100

    Анимация для партнеров

--- app/Components/Partners.tsx
+++ app/Components/Partners.tsx
@@ -1,51 +1,45 @@
-import React from 'react';
-import Styles from '../../public/Styles/partners.module.css';
+'use client'
+import React, { useState, useEffect } from 'react';
+import styles from '../../public/Styles/partners.module.css';
 import '../../public/Styles/globals.css';
 
-const Partners = () => {
+const Partners: React.FC = () => {
+    const [visibleLogos, setVisibleLogos] = useState<number[]>([0, 1, 2, 3, 4]);
+    const logoData = [
+        './partners/247.png',
+        './partners/betman.png',
+        './partners/chilli.png',
+        './partners/glory.png',
+        './partners/mostb.png',
+        './partners/mrbet.png',
+        './partners/v.png',
+    ];
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setVisibleLogos(prevVisibleLogos => 
+                prevVisibleLogos.map(index => (index + 1) % logoData.length)
+            );
+        }, 3000); // Rotate logos every 3 seconds
+
+        return () => clearInterval(interval);
+    }, []);
+
     return (
-        <div className={Styles.outerConatiner}>
-            <div className={Styles.mainContainer}>
+        <div className={styles.outerContainer}>
+            <div className={styles.mainContainer}>
                 <h1>Партнеры</h1>
             </div>
-            <div className={Styles.column}>
-                <div className={`${Styles.imageContainer} ${Styles.row}`}>
-                    {/* Each set now includes an outer frame image and an inner logo image */}
-                    <div className={Styles.logoContainer}>
-                        <img className={Styles.frameImage} src='./partners/elements/square(1).png' alt='Frame'></img>
-                        <img className={Styles.logoImage} src='./partners/247.png' alt='Лого компаний'></img>
+            <div className={styles.scrollContainer}>
+                {visibleLogos.map((index) => (
+                    <div key={index} className={`${styles.logoContainer} ${styles.fadeInRight}`}>
+                        <img className={styles.frameImage} src='./partners/elements/square(1).png' alt='Frame' />
+                        <img className={styles.logoImage} src={logoData[index]} alt='Logo' />
                     </div>
-                    <div className={Styles.logoContainer}>
-                        <img className={Styles.frameImage} src='./partners/elements/square(1).png' alt='Frame'></img>
-                        <img className={Styles.logoImage} src='./partners/betman.png' alt='Лого компаний'></img>
-                    </div>
-                    <div className={Styles.logoContainer}>
-                        <img className={Styles.frameImage} src='./partners/elements/square(1).png' alt='Frame'></img>
-                        <img className={Styles.logoImage} src='./partners/chilli.png' alt='Лого компаний'></img>
-                    </div>
-                </div>
-                <div className={`${Styles.imageContainer} ${Styles.row}`}>
-                    {/* Each set now includes an outer frame image and an inner logo image */}
-                    <div className={Styles.logoContainer}>
-                        <img className={Styles.frameImage} src='./partners/elements/square(1).png' alt='Frame'></img>
-                        <img className={Styles.logoImage} src='./partners/glory.png' alt='Лого компаний'></img>
-                    </div>
-                    <div className={Styles.logoContainer}>
-                        <img className={Styles.frameImage} src='./partners/elements/square(1).png' alt='Frame'></img>
-                        <img className={Styles.logoImage} src='./partners/mostb.png' alt='Лого компаний'></img>
-                    </div>
-                    <div className={Styles.logoContainer}>
-                        <img className={Styles.frameImage} src='./partners/elements/square(1).png' alt='Frame'></img>
-                        <img className={Styles.logoImage} src='./partners/mrbet.png' alt='Лого компаний'></img>
-                    </div>
-                    <div className={Styles.logoContainer}>
-                        <img className={Styles.frameImage} src='./partners/elements/square(1).png' alt='Frame'></img>
-                        <img className={Styles.logoImage} src='./partners/v.png' alt='Лого компаний'></img>
-                    </div>
-                </div>
+                ))}
             </div>
         </div>
-    )
+    );
 };
 
 export default Partners;
