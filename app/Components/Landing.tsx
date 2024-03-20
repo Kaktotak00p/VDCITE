d3c977e9d7364d8e4f26653a0da7a5b065fa912b
Kuzin, Mikle (Stud. DACS) <m.kuzin@student.maastrichtuniversity.nl>
2024-03-20 22:41:12 +0100

    поменял расположение кнопки на landing и добавил текст затычку

--- app/Components/Landing.tsx
+++ app/Components/Landing.tsx
@@ -15,11 +15,14 @@ return(
       <div className="tag">
         <span>[282 394]</span> ARBITRAGE TRAFFIC
       </div>
-      <button className={Style.button}>[ CLICK, ПОСМОТРЕТЬ ]</button>
     </div>
   </div>
   <div className={Style.imageContainer}>
     <img src="./partners/elements/makakich.png" alt="Media Buying Company" />
+    <div className={Style.column}>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus blanditiis, cumque nam sit unde velit delectus nobis enim voluptatum consequuntur.</p>
+      <button className={Style.button}>[ CLICK, ПОСМОТРЕТЬ ]</button>
+    </div>
   </div>
 </div>
   </div>
