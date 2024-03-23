eb2efb9afe2efd013a818f9735dc04c1556f8fe0
Kuzin, Mikle <m.kuzin@student.maastrichtuniversity.nl>
2024-03-23 21:01:56 +0100

    job hunt

--- app/Components/HeadHunter.tsx
+++ app/Components/HeadHunter.tsx
@@ -1,58 +1,48 @@
-import React from 'react';
+"use client"
+
+import React, { useState } from 'react';
 import Styles from '../../public/Styles/hh.module.css';
 import '../../public/Styles/globals.css';
 
+const jobListings = [
+  { title: 'Junior Media Buyer', description: 'Lorem ipsum dolor sit amet.' },
+  { title: 'Senior Media Buyer', description: 'Consectetur adipiscing elit.' },
+  // Add more job listings as needed
+];
+
 const HeadHunter = () => {
+    const [visibleDescription, setVisibleDescription] = useState(Array(jobListings.length).fill(false));
+
+    const toggleDescription = (index) => {
+        setVisibleDescription(visibleDescription.map((item, idx) => idx === index ? !item : item));
+    };
+
     return (
-        <div className={Styles.outerConatiner}>
-            <h1 className='mainText' style={{paddingLeft:"3em"}}>://HeadHunter</h1>
+        <div className={Styles.outerContainer}>
+            <h1 className='mainText' style={{ paddingLeft: "3em" }}>://HeadHunter</h1>
             <div className={Styles.mainContainer}>
                 <h1>Охота за головами</h1>
             </div>
             <div className={Styles.row}>
-            <img className={Styles.img} src='./partners/elements/005.png'></img>
+                <img className={Styles.img} src='./partners/elements/005.png' alt='Decorative' />
                 <div className={Styles.ol}>
-                <ol>
-                    <li className={Styles.layoutContainer}>
-                        <div className={`${Styles.row} ${Styles.entry}`}>
-                            <p className={Styles.p}>Junior Media Buyer</p>
-                            <p className={Styles.p}>Lorem ipsum dolor sit amet.</p>
-                            <button className={Styles.button}>Вакансия</button>
-                        </div>
-                    </li>
-                    <li className={Styles.layoutContainer}>
-                        <div className={`${Styles.row} ${Styles.entry}`}>
-                            <p className={Styles.p}>Junior Media Buyer</p>
-                            <p className={Styles.p}>Lorem ipsum dolor sit amet.</p>
-                            <button className={Styles.button}>Вакансия</button>
-                        </div>
-                    </li>
-                    <li className={Styles.layoutContainer}>
-                        <div className={`${Styles.row} ${Styles.entry}`}>
-                            <p className={Styles.p}>Junior Media Buyer</p>
-                            <p className={Styles.p}>Lorem ipsum dolor sit amet.</p>
-                            <button className={Styles.button}>Вакансия</button>
-                        </div>
-                    </li>
-                    <li className={Styles.layoutContainer}>
-                        <div className={`${Styles.row} ${Styles.entry}`}>
-                            <p className={Styles.p}>Junior Media Buyer</p>
-                            <p className={Styles.p}>Lorem ipsum dolor sit amet.</p>
-                            <button className={Styles.button}>Вакансия</button>
-                        </div>
-                    </li>
-                    <li className={Styles.layoutContainer}>
-                        <div className={`${Styles.row} ${Styles.entry}`}>
-                            <p className={Styles.p}>Junior Media Buyer</p>
-                            <p className={Styles.p}>Lorem ipsum dolor sit amet.</p>
-                            <button className={Styles.button}>Вакансия</button>
-                        </div>
-                    </li>
-                </ol>
+                    <ol>
+                        {jobListings.map((job, index) => (
+                            <li key={index} className={Styles.layoutContainer}>
+                                <div className={`${Styles.row} ${Styles.entry}`}>
+                                    <p className={`${Styles.p} ${Styles.jobName}`}>{job.title}</p>
+                                    <div className={`${Styles.description} ${visibleDescription[index] ? Styles.expanded : ''}`}>
+                                        <p className={Styles.p}>{job.description}</p>
+                                    </div>
+                                    <button className={Styles.button} onClick={() => toggleDescription(index)}>Вакансия</button>
+                                </div>
+                            </li>
+                        ))}
+                    </ol>
                 </div>
             </div>
         </div>
-    )
+    );
 };
 
 export default HeadHunter;
