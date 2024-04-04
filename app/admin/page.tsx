a656b09b568a85793825d008c2a06c2fd15d27a3
Kuzin, Mikle (Stud. DACS) <m.kuzin@student.maastrichtuniversity.nl>
2024-04-04 05:38:48 +0200

    multipage working

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,10 +1,13 @@
 import type { NextPage } from 'next'
+import RootLayout from '../layout';
 
 const Admin: NextPage = () => {
   return (
+    <RootLayout>
     <div>
       <h1>nothing to see here ... yet</h1>
     </div>
+    </RootLayout>
   )
 }
 
