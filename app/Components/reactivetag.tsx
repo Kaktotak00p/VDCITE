8b73b68f08a9f2bc7b774216b50007613f5c342a
Kuzin, Mikle <kaktotak00p@gmail.com>
2024-04-25 19:18:48 +0200

    backend

--- app/Components/reactivetag.tsx
+++ app/Components/reactivetag.tsx
@@ -9,7 +9,7 @@ const UserCountDisplay: React.FC = () => {
   useEffect(() => {
     const fetchCounter = async () => {
       try {
-        const response = await fetch(`http://nastoligr.com/api/counter`);
+        const response = await fetch(`https://nastoligr.com/api/counter`);
         const data = await response.json();
         setVlitoCounter(data.vlitoCounter);
       } catch (error) {
