80bfa737ce7bb28f1dad6df21106d565f9bb0582
Kuzin, Mikle <m.kuzin@student.maastrichtuniversity.nl>
2024-05-10 23:11:24 +0200

    pravki

--- app/Components/slidingPanel.tsx
+++ app/Components/slidingPanel.tsx
@@ -12,6 +12,7 @@ interface SlidingPanelProps {
 }
 
 const SlidingPanel: React.FC<SlidingPanelProps> = ({ isOpen, setIsOpen }) => {
+  const [showThankYou, setShowThankYou] = useState(false);
   const { t } = useTranslation();
   const [file, setFile] = useState<File | null>(null);
   const [formData, setFormData] = useState({
@@ -23,12 +24,12 @@ const SlidingPanel: React.FC<SlidingPanelProps> = ({ isOpen, setIsOpen }) => {
   });
 
   const handleChange: React.ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement> = (e) => {
-    const target = e.target as HTMLInputElement; 
+    const target = e.target as HTMLInputElement;
     if (target.type === 'file') {
-        const file = target.files && target.files.length > 0 ? target.files[0] : null;
-        setFile(file);
+      const file = target.files && target.files.length > 0 ? target.files[0] : null;
+      setFile(file);
     } else {
-        setFormData({ ...formData, [target.id]: target.value });
+      setFormData({ ...formData, [target.id]: target.value });
     }
   };
 
@@ -49,67 +50,70 @@ const SlidingPanel: React.FC<SlidingPanelProps> = ({ isOpen, setIsOpen }) => {
         headers: { 'Content-Type': 'multipart/form-data' },
       });
       console.log('Form successfully submitted:', response.data);
+      setShowThankYou(true);   // Show thank you message
+      setIsOpen(false);
+      setTimeout(() => setShowThankYou(false), 5000);
     } catch (error) {
       console.error('Error submitting form:', error);
     }
   };
 
-  return (
-<div className={`${styles.slidingPanel} ${isOpen ? styles.show : ''}`}>
-        <div className={styles.closeBtn} onClick={() => setIsOpen(false)}>&times;</div>
-        {/* <h1 style={{
+  return (<>
+    <div className={`${styles.slidingPanel} ${isOpen ? styles.show : ''}`}>
+      <div className={styles.closeBtn} onClick={() => setIsOpen(false)}>&times;</div>
+      {/* <h1 style={{
           margin: "1em"
         }}>ЗАПОЛНИТЕ ФОРМУ</h1> */}
-        <div className={styles.svgContainer} style={{
-          marginLeft:"-18em"
-        }}>
-          <img src='./svg/stick3.svg' className='svg-img' />
+      <div className={styles.svgContainer} style={{
+        marginLeft: "-18em"
+      }}>
+        <img src='./svg/stick3.svg' className='svg-img' />
+      </div>
+      <div className={styles.formheader}>
+        <img src='./dec/heart_contact.png' className={styles.heart} />
+      </div>
+      <form onSubmit={handleSubmit} className={`${styles.contactform} ${styles.row}`}>
+        <div className={styles.leftText}>
+          <h1 className={styles.formlogo}>{t("ContactForm.FillForm")}</h1>
+          <button type="submit" className={`${styles.button} ${styles.submitButton}`}>{t("ContactForm.submit")}</button>
         </div>
-        <div className={styles.formheader}>
-          <img src='./dec/heart_contact.png' className={styles.heart} />
-        </div>
-        <form onSubmit={handleSubmit} className={`${styles.contactform} ${styles.row}`}>
-          <div className={styles.leftText}>
-            <h1 className={styles.formlogo}>{t("ContactForm.FillForm")}</h1>
-            <button type="submit" className={`${styles.button} ${styles.submitButton}`}>{t("ContactForm.submit")}</button>
-          </div>
-          <div className={styles.singleLines}>
-            <ContactField
-              id="firstName"
-              label="First name"
-              type="text"
-              value={formData.firstName}
-              onChange={handleChange}
-            />
-            <ContactField
-              id="lastName"
-              label="Last name"
-              type="text"
-              value={formData.lastName}
-              onChange={handleChange}
-            />
-            <ContactField
-              id="email"
-              label="Email"
-              type="email"
-              value={formData.email}
-              onChange={handleChange}
-            />
-            <ContactField
-              id="telegramNickname"
-              label="Telegram Nickname"
-              type="text"
-              value={formData.telegramNickname}
-              onChange={handleChange}
-            />
-            <ContactField
-              id="message"
-              label="Message"
-              type="message"
-              value={formData.message}
-              onChange={handleChange}
-            />
-            <div className={styles.lowerCont}>
+        <div className={styles.singleLines}>
+          <ContactField
+            id="firstName"
+            label="First name"
+            type="text"
+            value={formData.firstName}
+            onChange={handleChange}
+          />
+          <ContactField
+            id="lastName"
+            label="Last name"
+            type="text"
+            value={formData.lastName}
+            onChange={handleChange}
+          />
+          <ContactField
+            id="email"
+            label="Email"
+            type="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          <ContactField
+            id="telegramNickname"
+            label="Telegram Nickname"
+            type="text"
+            value={formData.telegramNickname}
+            onChange={handleChange}
+          />
+          <ContactField
+            id="message"
+            label="Message"
+            type="message"
+            value={formData.message}
+            onChange={handleChange}
+          />
+          <div className={styles.lowerCont}>
             <button type="submit" className={`${styles.button} ${styles.submitButtonLower}`}>{t("ContactForm.submit")}</button>
             <ContactField
               id="file"
@@ -117,15 +121,20 @@ const SlidingPanel: React.FC<SlidingPanelProps> = ({ isOpen, setIsOpen }) => {
               type="file"
               value={formData.firstName}
               onChange={handleChange}
-              />
-              </div>
+            />
           </div>
-        </form>
-        <img className={styles.dec} src="./dec/logo/1.png" />
-        <div className={styles.footer}>
-          <h1>TRAFFHUB</h1>
         </div>
+      </form>
+      <img className={styles.dec} src="./dec/logo/1.png" />
+      <div className={styles.footer}>
+        <h1>TRAFFHUB</h1>
       </div>
+    </div>
+    {showThankYou && (
+      <div className={styles.thankYouMessage}>
+        <h1>Thank you for your submission!</h1>
+      </div>)}
+  </>
   );
 };
 
